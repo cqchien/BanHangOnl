4019e087fcc2cdf14d13e486cf531e4dd1f391e0
Chiến <caochientp1@gmail.com>
2020-04-28 11:29:30 +0700

    create components SmartPhone

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Carousel from "./components/Carousel";
 import Promotion from "./components/Promotion";
+import SmartPhone from "./components/SmartPhone";
 
 function App() {
   return <div className="App">
     <Nav />
     <Carousel />
+
+    <section id="smartphone" className="container-fluid pt-5 pb-5">
+      <h1 className="text-white text-center">BEST SMARTPHONE</h1>
+      <div className="row">
+        <SmartPhone />
+      </div>
+    </section>
+
     <Promotion />
   </div>;
 }
