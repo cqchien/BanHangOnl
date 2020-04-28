e04ccf2564e5b24a4b4ddcf5dae7f4afddb0f418
Chiến <caochientp1@gmail.com>
2020-04-28 12:25:12 +0700

    mockup data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./App.css";
 // * Import Components
@@ -7,20 +7,41 @@ import Carousel from "./components/Carousel";
 import Promotion from "./components/Promotion";
 import SmartPhone from "./components/SmartPhone";
 
-function App() {
-  return <div className="App">
-    <Nav />
-    <Carousel />
+//* Import Data
+import SmartPhoneData from "./data/SmartPhone";
 
-    <section id="smartphone" className="container-fluid pt-5 pb-5">
-      <h1 className="text-white text-center">BEST SMARTPHONE</h1>
-      <div className="row">
-        <SmartPhone />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spInfo: SmartPhoneData,
+    };
+  }
+  render() {
+    let smartphone = this.state.spInfo.map((phone, index) => {
+      return (
+        <SmartPhone
+          name={phone.name}
+          desc={phone.desc}
+          img={phone.img}
+          price={phone.price}
+          key={index}
+        />
+      );
+    });
+    return (
+      <div className="App">
+        <Nav />
+        <Carousel />
+
+        <section id="smartphone" className="container-fluid pt-5 pb-5">
+          <h1 className="text-white text-center">BEST SMARTPHONE</h1>
+          <div className="row">{smartphone}</div>
+        </section>
+        <Promotion />
       </div>
-    </section>
-
-    <Promotion />
-  </div>;
+    );
+  }
 }
 
 export default App;
--- src/components/SmartPhone.jsx
+++ src/components/SmartPhone.jsx
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 
 class SmartPhone extends Component {
   render() {
+    let {name, desc, img, price } = this.props
     return (
       <div>
-        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-lg-3">
-          <div className="container">
-            <div className="card bg-light" style={{width: 300}}>
-              <img className="card-img-top" src="./img/sp_iphoneX.png" alt="Card image" style={{maxWidth: '100%', height: 250}} />
+        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3 pb-3 pt-5">
+          <div className="container"> 
+            <div className="card bg-light" style={{width: 290}}>
+              <img className="card-img-top" src={img} alt="Card image" style={{maxWidth: '100%', height: 250}} />
               <div className="card-body text-center">
-                <h4 className="card-title text-center">IPhone X</h4>
-                <p className="card-text">IPhone X features a new all-screen design. Face ID, which makes your face your password</p>
+              <h4 className="card-title text-center">{name}</h4>
+              <p className="card-text">{price}</p>
+              <p className="card-text">{desc}</p>
                 <a href="#" className="btn btn-primary">Detail</a>
                 <a href="#" className="btn btn-danger">Cart</a>
               </div>
